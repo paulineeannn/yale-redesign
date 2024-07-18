efbd53b1eb35398a31ba3b8dca30eb99bc6da7d4
Pauline Ann Bautista <paulineannbautista@gmail.com>
2024-07-19 04:37:09 +0800

    auto increment for inquiry id

--- soa_inquiries.sql
+++ soa_inquiries.sql
@@ -1,7 +1,15 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3307:3307
+-- Generation Time: Jul 18, 2024 at 10:29 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = '+08:00';
-
+SET time_zone = "+08:00";
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -19,15 +27,17 @@ SET time_zone = '+08:00';
 --
 
 CREATE TABLE `soa_inquiries` (
-  `inquiry_id` int(11) NOT NULL,
+  `inquiry_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `contact` varchar(20) NOT NULL,
   `email` varchar(255) NOT NULL,
   `message` text NOT NULL,
-  `submit_time` timestamp NOT NULL DEFAULT current_timestamp()
+  `submit_time` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`inquiry_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+COMMIT;
